c5564b018c8a4819512b0feb062f071f49d2bdcd
awantech-io <hello.wanlab@gmail.com>
2020-10-19 10:44:05 +0800

    complete v1.0.0

--- src/App.js
+++ src/App.js
@@ -1,112 +1,37 @@
-import React, { Fragment, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import User from './components/users/User';
 import './App.css';
-import axios from 'axios';
 import Alert from './components/layout/Alert';
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 
 const App = () => {
-
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-  const [repos, setRepos] = useState([]);
   
-    // Search Github users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(
-    `https://api.github.com/search/users?q=${text}&client_id=$
-    {process.env.REACT_APP_GITHUB_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
-  // get single github user
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-    `https://api.github.com/users/${username}?client_id=$
-    {process.env.REACT_APP_GITHUB_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUser(res.data);
-    setLoading(false);
-  }
-
-  // get user repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-    `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
-    {process.env.REACT_APP_GITHUB_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setRepos(res.data);
-    setLoading(false);
-  }
-
-  // clear user from state when submit clear button
-  const clearUsers = () => { 
-    setUsers([]); 
-    setLoading(false);
-  }
-
-  // set alert message 
- const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout( () => setAlert(null), 5000);
-  };
-
     return (
       <GithubState>
+      <AlertState>
       <Router>
       <div>
         <Navbar />
         <div className='container'>
-          <Alert alert={alert} />
+          <Alert />
           <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                <Search 
-                  searchUsers={searchUsers} 
-                  clearUsers={clearUsers} 
-                  showClear={users.length > 0 ? true : false}
-                  setAlert={showAlert}
-                />
-                <Users loading={loading} users={users} />
-              </Fragment>
-            )} />
-
+            <Route exact path='/' component={Home} />
             <Route exact path='/about' component={About} />
-
-            <Route exact path='/user/:login' render={ props => ( 
-            <User { ...props } 
-            getUser={getUser} 
-            user={user} 
-            loading={loading} 
-            getUserRepos={getUserRepos}
-            repos={repos}
-            />
-            )}/>
-
+            <Route exact path='/user/:login' component={User}/>
+            <Route component={NotFound} />
           </Switch>
         
         </div>
       </div>
       </Router>
+      </AlertState>  
       </GithubState>
       );
   
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -6,7 +6,7 @@ import {
     SEARCH_USERS,
     SET_LOADING,
     CLEAR_USERS,
-    GET_USERS,
+    GET_USER,
     GET_REPOS
 }  from '../types';
 
@@ -20,22 +20,70 @@ const GithubState = props => {
 
     const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-    // search users
+    // Search Github users
+  const searchUsers = async text => {
+    setLoading(true);
 
-    // get user
+    const res = await axios.get(
+    `https://api.github.com/search/users?q=${text}&client_id=$
+    {process.env.REACT_APP_GITHUB_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    // get repos
+    dispatch({
+        type: SEARCH_USERS,
+        payload: res.data.items
+    });
+    
+  };
 
-    // clear users
+    // get single github user
+    const getUser = async (username) => {
+        setLoading();
+
+        const res = await axios.get(
+        `https://api.github.com/users/${username}?client_id=$
+        {process.env.REACT_APP_GITHUB_ID}&client_secret=$
+        {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+        dispatch({
+            type: GET_USER,
+            payload: res.data
+        });
+
+    }
+
+    // get user repos
+    const getUserRepos = async (username) => {
+        setLoading();
+
+        const res = await axios.get(
+        `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
+        {process.env.REACT_APP_GITHUB_ID}&client_secret=$
+        {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        });
+
+    }
+
+    // clear user from state when submit clear button
+    const clearUsers = () => dispatch ({ type: CLEAR_USERS });
 
     // set loading
+    const setLoading = () => dispatch({ type: SET_LOADING });
 
     return <GithubContext.Provider
         value={{
             users: state.users,
             user: state.user,
             repos:state.repos,
-            loading: state.loading
+            loading: state.loading,
+            searchUsers,
+            clearUsers,
+            getUser,
+            getUserRepos
         }}
     >
         {props.children}
